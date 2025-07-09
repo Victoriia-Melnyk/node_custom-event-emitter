81fa67d90e8248e32db81378f165e4c823810ecf
Viktoriia Melnyk <melnychka1993@gmail.com>
2025-07-09 21:04:29 +0100

    done listener

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,60 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this.eventsArray = [];
+  }
+
+  on(eventName, eventCallback) {
+    this.eventsArray.push({ eventName, eventCallback });
+  }
+  once(eventName, eventCallback) {
+    this.eventsArray.push({ eventName, eventCallback, once: true });
+  }
+  off(eventName, eventCallback) {
+    this.eventsArray = this.eventsArray.filter(
+      (event) =>
+        !(
+          event.eventName === eventName && event.eventCallback === eventCallback
+        ),
+    );
+  }
+
+  emit(eventName, ...args) {
+    const eventArray = this.eventsArray.filter(
+      (item) => item.eventName === eventName,
+    );
+
+    eventArray.forEach((filteredElement) => {
+      filteredElement.eventCallback(...args);
+    });
+
+    this.eventsArray = this.eventsArray.filter(
+      (event) => !(event.once && event.eventName === eventName),
+    );
+  }
+  prependListener(eventName, eventCallback) {
+    this.eventsArray = [{ eventName, eventCallback }, ...this.eventsArray];
+  }
+  prependOnceListener(eventName, eventCallback) {
+    this.eventsArray = [
+      { eventName, eventCallback, once: true },
+      ...this.eventsArray,
+    ];
+  }
+  removeAllListeners(eventName) {
+    if (eventName) {
+      this.eventsArray = this.eventsArray.filter(
+        (event) => event.eventName !== eventName,
+      );
+    } else {
+      this.eventsArray = [];
+    }
+  }
+  listenerCount(eventName) {
+    return this.eventsArray.filter((event) => event.eventName === eventName)
+      .length;
+  }
 }
 
 module.exports = MyEventEmitter;
